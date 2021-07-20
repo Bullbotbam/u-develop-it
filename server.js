5eaf37786a3c54c5ccf9476dc816a9032edb211e
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-07-20 06:41:51 -0500

    fulfilled requests

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2')
 const express = require('express');
+const { syncBuiltinESMExports } = require('module');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -25,10 +26,35 @@ const db = mysql.createConnection(
 app.use((req, res) => {
     res.status(404).end();
   });
-  db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
+//   db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//   });
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(result);
+// });
 
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
