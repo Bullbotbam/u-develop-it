c7bfa44a89ab45f1e50dc02e69b0b5ef9b4abb32
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-07-21 17:36:20 -0500

    create get route for voter

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const db = require('./db/connection');
 const express = require('express');
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
@@ -8,9 +8,11 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
@@ -18,7 +20,7 @@ app.use((req, res) => {
 // Start server after DB connection
 db.connect(err => {
   if (err) throw err;
-  console.log('Database connected.');
+  console.log('Your Database is connected.');
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
